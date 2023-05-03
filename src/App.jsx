b3b3142aeb2202b260771fefcbbdab0f2d6581ca
M-Usman-Nadeem <usmanprg9@gmail.com>
2023-05-03 20:44:55 +0500

    weather-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+import ReactSwitch from "react-switch";
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [weatherData, setWeatherData] = useState([]);
+  useEffect(() => {
+    callingApi();
+  }, [checked]);
+  const handleChange = (val) => {
+    setChecked(!checked);
+  };
+
+  async function callingApi() {
+    if (inputValue !== "") {
+      try {
+        const { data } = await axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&cnt=5&units=${
+            checked ? "metric" : "imperial"
+          }&appid=337eaa2ba65564479244ff2634a42d1f`
+        );
+        setWeatherData(data?.list);
+      } catch (err) {}
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <form className="input-wrapper">
+
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+          placeholder="Enter location"
+        />
+        <div className="toggle-switch" >
+          <p>Farenhit</p>{" "}
+          <ReactSwitch
+            checked={checked}
+            onChange={handleChange}
+            onColor="#86d3ff"
+            onHandleColor="#2693e6"
+            handleDiameter={30}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+            height={20}
+            width={48}
+            className="react-switch"
+            id="material-switch"
+          />{" "}
+          <p>Celcius</p>
+        </div>
+      </form>
+      <button className="search-button" onClick={callingApi}>
+        Search
+      </button>
+
+      <div className="weather-data-wrapper">
+        {weatherData.map((item) => {
+          console.log();
+          return (
+            <div className="weather-item">
+              <img src={`http://openweathermap.org/img/w/${item?.weather[0]?.icon}.png`} alt="" />
+              <div>Temperature</div>
+              <div className="temp">{item?.main?.temp} {checked?'Farenhit':'Celcius'}</div>
+              <div>Humidity</div>
+              <div className="humidity">{item?.main?.humidity}</div>
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
